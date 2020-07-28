3f6596c7cd480ed667f6ff87cb16afe9ab164674
duy <tanduyht@gmail.com>
2020-07-28 14:02:47 +0700

    clear config for db

--- src/uit/connection/DBConnection.java
+++ src/uit/connection/DBConnection.java
@@ -8,9 +8,6 @@ public class DBConnection {
 	public static Connection getConnection() {
 		Connection con = null;
 		try {
-//			String url = "jdbc:sqlserver://uit:1433;databaseName=SOF301_Assignment";
-//			Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
-//			con = DriverManager.getConnection(url, "uit", "123");
 			Class.forName("com.mysql.jdbc.Driver");
 			String url = "jdbc:mysql://localhost:3306/shop_manage";
 			
